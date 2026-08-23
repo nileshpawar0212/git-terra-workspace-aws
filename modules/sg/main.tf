3d3aec4c5b3dc910fe4d9c601d1eb2dd14c758a9
Nilesh Pawar <nileshpawar0212@gmail.com>
2026-08-23 11:32:57 +0000

    error solved

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -3,14 +3,6 @@ resource "aws_security_group" "cluster" {
   description = "EKS cluster control plane SG"
   vpc_id      = var.vpc_id
 
-  ingress {
-    description     = "Allow nodes to reach API server"
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    security_groups = [aws_security_group.node.id]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -26,22 +18,6 @@ resource "aws_security_group" "node" {
   description = "EKS node group SG"
   vpc_id      = var.vpc_id
 
-  ingress {
-    description = "Allow nodes to communicate with each other"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    self        = true
-  }
-
-  ingress {
-    description     = "Allow control plane to reach kubelet"
-    from_port       = 1025
-    to_port         = 65535
-    protocol        = "tcp"
-    security_groups = [aws_security_group.cluster.id]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -50,4 +26,38 @@ resource "aws_security_group" "node" {
   }
 
   tags = merge(var.tags, { Name = "${var.name}-eks-node-sg" })
+}
+
+# --- Cluster SG rules ---
+
+resource "aws_security_group_rule" "cluster_from_node" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.cluster.id
+  source_security_group_id = aws_security_group.node.id
+  description              = "Allow nodes to reach API server"
+}
+
+# --- Node SG rules ---
+
+resource "aws_security_group_rule" "node_self" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  self              = true
+  security_group_id = aws_security_group.node.id
+  description       = "Allow nodes to communicate with each other"
+}
+
+resource "aws_security_group_rule" "node_from_cluster" {
+  type                     = "ingress"
+  from_port                = 1025
+  to_port                  = 65535
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.node.id
+  source_security_group_id = aws_security_group.cluster.id
+  description              = "Allow control plane to reach kubelet and pods"
 }
