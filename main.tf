b095c130485f6ff88cc68941efbb9ad4edeefea6
Nilesh Pawar <nileshpawar0212@gmail.com>
2026-08-23 08:11:23 +0000

    change vpc name

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "./modules/vpc"
 
-  name                 = "my-vpc"
+  name                 = "demo-eks-vpc"
   vpc_cidr             = "10.0.0.0/16"
   azs                  = ["us-east-1a", "us-east-1b"]
   public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
